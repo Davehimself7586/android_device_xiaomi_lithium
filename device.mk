d6f1be7eb91f8f2a40518f22103082d5adb0883d
RMarchetto96 <rafagossmann@gmail.com>
2017-10-22 17:35:48 +0530

    Revert "lithium: Move Lineage specific overlays to overlay-lineage"
    This reverts commit c19fa2ade75c56fdc90c35954addadce59f5332a.

--- device.mk
+++ device.mk
@@ -17,10 +17,8 @@
 # call the proprietary setup
 $(call inherit-product, vendor/xiaomi/lithium/lithium-vendor.mk)
 
-# Local overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Audio
 PRODUCT_COPY_FILES += \
